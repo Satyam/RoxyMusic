7543016646484ac42a83a7048c22a8f33e5dfb2d
Satyam <satyam@satyam.com.ar>
2016-11-06 15:03:21 +0100

    missed in last commit

--- client/components/albums/album.jsx
+++ client/components/albums/album.jsx
@@ -1,22 +1,39 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Icon from '_utils/icon';
 import compose from 'recompose/compose';
-import isEmpty from 'lodash/isEmpty';
-import { getAlbum } from '_store/actions';
+import { getAlbum, getAlbumTracks } from '_store/actions';
 import initStore from '_utils/initStore';
 import TrackList from '_components/tracks/trackList';
 import styles from './album.css';
 
-export const AlbumComponent = ({ idAlbum, album, artists, numTracks, idTracks }) =>
+export const AlbumComponent = ({ idAlbum, album, artists, numTracks, idTracks, error }) =>
   (idAlbum || null) && (
-    <div className={styles.album}>
-      <h1>{album}</h1>
-      <div className={styles.albumInfo}>
-        <div className={styles.albumArtist}>{artists}</div>
-        <div className={styles.albumNumTracks}>{numTracks}</div>
+    error === 404
+    ? (<div className={styles.notFound}>Album for that URL no longer exists</div>)
+    : (
+      <div className={styles.album}>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link href="/"><Icon type="arrow-up" /> {album}</Link>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <div className={styles.albumInfo}>
+              <div className={styles.albumArtist}>{artists}</div>
+              <div className={styles.albumNumTracks}>
+                {numTracks} {numTracks === 1 ? 'track' : 'tracks'}
+              </div>
+            </div>
+          </Navbar.Collapse>
+        </Navbar>
+        <TrackList idTracks={idTracks} />
       </div>
-      <TrackList idTracks={idTracks} />
-    </div>
+    )
   );
 
 
@@ -28,23 +45,25 @@ AlbumComponent.propTypes = {
   idTracks: PropTypes.arrayOf(
     PropTypes.number
   ),
+  error: PropTypes.number,
 };
 
 export const storeInitializer = (dispatch, state, props) => {
   const idAlbum = props.params.idAlbum;
-  if (!state.albums[idAlbum] || isEmpty(state.albums[idAlbum].tracks)) {
-    return dispatch(getAlbum(idAlbum));
-  }
-  return undefined;
+  const album = state.albums.albumHash[idAlbum];
+  return Promise.all([
+    album || dispatch(getAlbum(idAlbum)),
+    (album && album.idTracks) || dispatch(getAlbumTracks(idAlbum)),
+  ]);
 };
 
-export const mapStateToProps = (state, props) => state.albums[props.params.idAlbum] || {};
+export const mapStateToProps = (state, props) => state.albums.albumHash[props.params.idAlbum] || {};
 
 const enhance = compose(
+  initStore(storeInitializer),
   connect(
     mapStateToProps
-  ),
-  initStore(storeInitializer)
+  )
 );
 
 export default enhance(AlbumComponent);
