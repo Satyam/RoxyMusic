4de34c276f2701975580b07baf661f829a3adcdb
Satyam <satyam@satyam.com.ar>
2016-11-11 18:12:49 +0100

    adding validators

--- server/music/validators.js
+++ server/music/validators.js
@@ -1,1 +1,26 @@
+import { failRequest } from '_server/utils';
 
+const testIdTracks = /^\d+(,\d+)*$/;
+const testInteger = /^\d+$/;
+
+/* eslint-disable no-param-reassign, consistent-return */
+export function validateIdTracks(o) {
+  const idTracks = o.keys.idTracks;
+  if (idTracks && !testIdTracks.test(idTracks)) {
+    return failRequest(400, 'Bad Request');
+  }
+}
+
+export function validateIdAlbum(o) {
+  const idAlbum = o.keys.idAlbum;
+  if (idAlbum && !testInteger.test(idAlbum)) {
+    return failRequest(400, 'Bad Request');
+  }
+}
+
+export function validateIdPlayList(o) {
+  const idPlayList = o.keys.idPlayList;
+  if (idPlayList && !testInteger.test(idPlayList)) {
+    return failRequest(400, 'Bad Request');
+  }
+}
