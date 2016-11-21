1bbcc238f592c33e3874aea5702e075d1936907b
Satyam <satyam@satyam.com.ar>
2016-11-21 17:50:50 +0100

    added loging

--- server/utils/handleRequest.js
+++ server/utils/handleRequest.js
@@ -1,4 +1,9 @@
+import debug from 'debug';
+
 export const failRequest = (code, message) => Promise.reject({ code, message });
+
+// debug.enable('RoxyMusic:handleRequest');
+const log = debug('RoxyMusic:handleRequest');
 
 export const handleRequest = (...args) => (req, res) => {
   const o = {
@@ -7,10 +12,14 @@ export const handleRequest = (...args) => (req, res) => {
     options: req.query || {},
   };
 
+  log('> %s %j', req.url, o);
   const action = args[args.length - 1];
   Promise.all(args.slice(0, -1).map(validator => validator(o)))
   .then(() => action(o))
-  .then(reply => res.json(reply))
+  .then((reply) => {
+    log('< %s %j', req.url, reply);
+    return res.json(reply);
+  })
   .catch((reason) => {
     res.status((reason instanceof Error) ? 500 : reason.code).send(reason.message);
   });
