025e16455574985238e2ffd5a31f771becc3ee61
Satyam <satyam@satyam.com.ar>
2016-12-11 18:47:14 +0100

    One of many failed trials

--- phonegap/index.js
+++ phonegap/index.js
@@ -1,29 +1,85 @@
 import client from '_client';
-import { join } from 'path';
-
-import fs from 'fs';
-import denodeify from 'denodeify';
-import openDatabase from '_server/utils/openCordovaWebSql';
+// import { join } from 'path';
+// import fs from 'fs';
+import openDatabase from '_server/utils/openCordovaWebSqlevcore';
 import DB from '_server/utils/webSqlP';
 
 import dataServers from '_server';
 
 import { addRoute } from './restAPI';
 
-const unlink = denodeify(fs.unlink);
-const readFile = denodeify(fs.readFile);
+const resolveLocalFileSystemURL = path => new Promise((resolve, reject) =>
+  window.resolveLocalFileSystemURL(
+    path,
+    resolve,
+    (err) => {
+      reject(`resolveLocalFileSystemURL for ${path} returned ${JSON.stringify(err)}`);
+    }
+  )
+);
+function readFile(path, encoding) {
+  return resolveLocalFileSystemURL(path)
+  .then(fileEntry => new Promise((resolve, reject) => {
+    fileEntry.file(
+      (file) => {
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+          resolve(reader.result);
+        };
+        reader.onerror = (ev) => {
+          reject(`readAsText for ${path} returned ${JSON.stringify(ev)}`);
+        };
+        reader.readAsText(file, encoding);
+      },
+      (err) => {
+        reject(`readFile for ${path} returned ${JSON.stringify(err)}`);
+      }
+    );
+  }));
+}
+
+// function unlink(path) {
+//   return resolveLocalFileSystemURL(path)
+//   .then(fileEntry => new Promise(fileEntry.remove));
+// }
+
 
 window.addEventListener('load', () => {
   const el = document.getElementById('log');
   function log(msg) {
     el.innerHTML = `${el.innerHTML}<pre>${msg}</pre>`;
-    navigator.notification.alert(
-      msg,  // message
-      () => {},         // callback
-      'Game Over',            // title
-      'Done'                  // buttonName
-    );
+    // navigator.notification.alert(
+    //   msg,  // message
+    //   () => {},         // callback
+    //   'Game Over',            // title
+    //   'Done'                  // buttonName
+    // );
   }
+  // const listDir = (key, path, howMany = 10) =>
+  //   resolveLocalFileSystemURL(path)
+  //   .then(dir => new Promise((resolve, reject) => {
+  //     if (dir.createReader) {
+  //       const directoryReader = dir.createReader();
+  //       directoryReader.readEntries(
+  //         (results) => {
+  //           log(`For ${key}, path: ${path} found ${results.length} entries`);
+  //           results.slice(0, howMany).forEach((fileEntry) => {
+  //             log(`${key} ${fileEntry.isFile ? 'F' : 'D'}, path ${fileEntry.fullPath},
+  //  ${fileEntry.toInternalURL()}`);
+  //           });
+  //           resolve(dir);
+  //         },
+  //         reject
+  //       );
+  //     } else {
+  //       log(`${key} ${dir.name} does not have createReader`);
+  //       reject(`${key} ${dir.name} does not have createReader`);
+  //     }
+  //   }))
+  //   .catch((err) => {
+  //     log(`Failed to resolve ${key}, ${path}, err: ${JSON.stringify(err)}`);
+  //   });
 
   // log('comenzando');
 
@@ -31,7 +87,6 @@ window.addEventListener('load', () => {
     'deviceready',
     () => {
       if (!el) {
-        alert('el not found');
         navigator.notification.alert(
           'el not found',  // message
           () => {},         // callback
@@ -41,35 +96,63 @@ window.addEventListener('load', () => {
       }
       log('arranque');
       const DELDB = false;
-      const musicDb = join(cordova.file.externalRootDirectory, 'Music/RoxyMusic.db');
-
-      log(`musicdb: ${musicDb}`);
-      (DELDB ? unlink(musicDb) : Promise.resolve())
-      .then(() => openDatabase(musicDb))
+      // const musicDb = join(cordova.file.externalRootDirectory || '/', 'Music/RoxyMusic.db');
+      //
+      // log(`musicdb: ${musicDb}`);
+      // if (fs && fs.init) {
+      //   fs.init(null, (err, granted) => {
+      //     if (err) {
+      //       log(`fs.init error ${JSON.stringify(err)}`);
+      //     } else {
+      //       log(`Fs.init granted ${granted}`);
+      //       if (fs.readdir) {
+      //         fs.readdir(
+      //           '/',
+      //           (err1, files) => {
+      //             if (err) {
+      //               log(`fs.readdir error: ${JSON.stringify(err1)}`);
+      //             } else {
+      //               log(`fs.readdir found ${files.length} entries`);
+      //               log(files.map(entry => entry.name).join(',\n'));
+      //             }
+      //           }
+      //         );
+      //       } else log(`fs or fs.readdir not found: ${typeof fs}, ${typeof fs.readdir}`);
+      //     }
+      //   });
+      // }
+      window.sqlitePlugin.echoTest(() => {
+        log('sqlitePlugin ECHO test OK');
+      });
+      window.sqlitePlugin.selfTest(() => {
+        log('sqlitePlugin SELF test OK');
+      });
+      openDatabase('RoxyMusic.db') // (musicDb))
       .then((rawDb) => {
         log('database opened');
         return new DB(rawDb);
       })
-      .then(db =>
-        db.get('select count(*) as c  from sqlite_master')
-        .then((row) => {
-          log(`red row ${JSON.stringify(row)}`);
-          return (
-            row.c === 0
-            ? readFile(join(cordova.file.applicationDirectory, 'res/data.sql'), 'utf8')
-              .then((data) => {
-                log(`data read ${data.substr(0, 200)}`);
-                return db.exec(data);
-              })
-              .then(() => db)
-            : db
-          );
-        })
-      )
+      .then((db) => {
+        function initDb() {
+          return readFile(`${cordova.file.applicationDirectory}/www/res/data.sql`, 'utf8')
+            .then((data) => {
+              log(`data read ${data.substr(0, 200)}`);
+              return db.exec(data);
+            })
+            .then(() => db);
+        }
+        if (DELDB) return initDb();
+        return db.get('select count(*) as c from config')
+        .then(() => db)
+        .catch((err1) => {
+          log(`catch row ${JSON.stringify(err1)}`);
+          return initDb();
+        });
+      })
       .then(db => dataServers(db, addRoute))
       .then(client)
       .catch((err) => {
-        log(`catch: ${err}`);
+        log(`catch: ${JSON.stringify(err)}`);
       });
     },
     false
