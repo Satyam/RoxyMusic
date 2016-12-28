a60ac0e128e6c17b27c3613051454e3928d5086b
Satyam <satyam@satyam.com.ar>
2016-12-28 19:26:23 +0100

    loads local and remote playlists and shows progress.

--- client/components/sync/sync.jsx
+++ client/components/sync/sync.jsx
@@ -3,22 +3,42 @@ import { connect } from 'react-redux';
 import { compose } from 'recompose';
 
 import Icon from '_components/misc/icon';
+import Alert from 'react-bootstrap/lib/Alert';
+import initStore from '_utils/initStore';
+import { storeInitializer } from '_components/playLists/playLists';
+
 import { startSync, getDifferences } from '_store/actions';
 
-export function SyncComponent({ onSyncStart, uuid, idDevice, list }) {
+
+const messages = [
+  '',
+  'idDevice received',
+  'Current server playlists received',
+];
+
+export function SyncComponent({
+  stage,
+  onSyncStart,
+  uuid,
+  idDevice,
+  list,
+}) {
   return (<div>
     <Icon
       type="retweet"
       className="btn btn-default"
       onClick={onSyncStart}
       label="Start"
+      disabled={stage !== 0}
     />
+    <Alert bsStyle="info">{messages[stage]}</Alert>
     <p>{`uuid: ${uuid}, idDevice: ${idDevice}`}</p>
     <ul>
       {list.map(item => (
-        <li>{item.idPlayList}: ({item.idPlayListHistory}) <em>{item.currentName}</em>
+        <li key={item.idPlayList}>
+          {item.idPlayList}: ({item.idPlayListHistory}) <strong>{item.currentName}</strong>
           <ul>
-            {item.currentIdTracks.split(',').map(idTrack => (<li>{idTrack}</li>))}
+            {item.currentIdTracks.split(',').map(idTrack => (<li key={idTrack}>{idTrack}</li>))}
           </ul>
         </li>
       ))}
@@ -31,6 +51,7 @@ SyncComponent.propTypes = {
   uuid: PropTypes.string,
   idDevice: PropTypes.number,
   list: PropTypes.array,
+  stage: PropTypes.number,
 };
 
 export const mapStateToProps = state => state.sync;
@@ -41,7 +62,9 @@ export const mapDispatchToProps = dispatch => ({
     .then(action => dispatch(getDifferences(action.payload.idDevice))),
 });
 
+
 export default compose(
+  initStore(storeInitializer),
   connect(
     mapStateToProps,
     mapDispatchToProps
