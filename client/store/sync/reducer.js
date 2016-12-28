4d196e240da6525112b8f3013653dc33ec88642f
Satyam <satyam@satyam.com.ar>
2016-12-28 19:25:02 +0100

    sync keeps track of the stage the process is in

--- client/store/sync/reducer.js
+++ client/store/sync/reducer.js
@@ -1,6 +1,7 @@
 import update from 'react-addons-update';
 
 import {
+  REQUEST_SENT,
   REPLY_RECEIVED,
 } from '_store/requests/actions';
 
@@ -14,18 +15,33 @@ export default (
     uuid: null,
     idDevice: null,
     list: [],
+    stage: 0,
   },
   action
 ) => {
   const payload = action.payload;
   // const list = payload && payload.list;
-
-  if (action.stage !== REPLY_RECEIVED) return state;
-  switch (action.type) {
-    case START_SYNC:
-      return update(state, { $merge: payload });
-    case GET_DIFFERENCES:
-      return update(state, { $merge: payload });
+  switch (action.stage) {
+    case REQUEST_SENT:
+      switch (action.type) {
+        default:
+          return state;
+      }
+    case REPLY_RECEIVED:
+      switch (action.type) {
+        case START_SYNC:
+          return update(state, {
+            $merge: payload,
+            stage: { $set: 1 },
+          });
+        case GET_DIFFERENCES:
+          return update(state, {
+            $merge: payload,
+            stage: { $set: 2 },
+          });
+        default:
+          return state;
+      }
     default:
       return state;
   }
