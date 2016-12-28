0269c6dee423ed7c0df38cba22f26f989ae03f5d
Satyam <satyam@satyam.com.ar>
2016-12-28 19:08:11 +0100

    sync actions can be executed on local or remote server

--- client/store/sync/actions.js
+++ client/store/sync/actions.js
@@ -1,10 +1,12 @@
 import restAPI from '_platform/restAPI';
+import remoteAPI from '_client/../webClient/restAPI';
 import asyncActionCreator from '_utils/asyncActionCreator';
 
 const NAME = 'sync';
+const rApi = remoteAPI(NAME, 'http://192.168.0.101');
 const api = restAPI(NAME);
 
-const UUID = 'my fake uuid';
+const UUID = `${window.device.model} : ${window.device.uuid}`;
 
 export const START_SYNC = `${NAME}/Start Synchronization`;
 export const GET_DIFFERENCES = `${NAME}/get differences`;
@@ -12,15 +14,15 @@ export const GET_DIFFERENCES = `${NAME}/get differences`;
 export function startSync() {
   return asyncActionCreator(
     START_SYNC,
-    api.read(`/myId/${UUID}`),
+    rApi.read(`/myId/${UUID}`),
     { uuid: UUID }
   );
 }
 
-export function getDifferences() {
+export function getDifferences(idDevice) {
   return asyncActionCreator(
     GET_DIFFERENCES,
-    api.read('/differences/2'),
-    { idDevice: 2 }
+    rApi.read(`/differences/${idDevice}`),
+    { idDevice }
   );
 }
