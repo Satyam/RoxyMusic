646fae6e70be8e10f5b52d3f3ac0cc22935e821f
Satyam <satyam@satyam.com.ar>
2017-01-19 12:50:52 +0100

    plainJoin now does not strip out slashes at either end

--- client/utils/plainJoin.js
+++ client/utils/plainJoin.js
@@ -1,7 +1,14 @@
 const initial = /^\//;
 const final = /\/$/;
 
-export default (...paths) =>
-  paths.map(
-    path => String(path).replace(initial, '').replace(final, '')
+export default (...paths) => {
+  const last = paths.length - 1;
+  return paths.map(
+    (path, index) => {
+      let p = String(path);
+      if (index) p = p.replace(initial, '');
+      if (index < last) p = p.replace(final, '');
+      return p;
+    }
   ).join('/');
+};
