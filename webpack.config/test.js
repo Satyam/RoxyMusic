52f95619d3dbd4f550ec1b5f0290adebde980f1e
Satyam <satyam@satyam.com.ar>
2017-02-01 19:55:09 +0100

    Wbpack config for test was not used.

--- webpack.config/test.js
+++
@@ -1,8 +0,0 @@
-const config = require('./common')('development')[1];
-
-config.entry = null;
-config.output = null;
-config.target = 'node';
-config.devtool = 'source-map';
-
-module.exports = config;
