3678041974301d9b14657418fec17f0f844741e1
Satyam <satyam@satyam.com.ar>
2017-02-12 17:31:03 +0100

    dropped route not in use

--- client/components/songs/routes.jsx
+++ client/components/songs/routes.jsx
@@ -6,7 +6,6 @@ import SongList from './songList';
 
 export default path => (
   <Route>
-    <Route path={`${path}/:idTrack`} component={Track} />
     <Route path={path} component={SongList} />
   </Route>
 );
